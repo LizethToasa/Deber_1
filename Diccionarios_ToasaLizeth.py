a8789fbc363dce09ded5968356dd1f67f6e4c96d
LizethToasa <lizeth.toasa@gmail.com>
2016-11-16 13:46:20 -0500

    Diccinario de telefonos

---
+++ Diccionarios_ToasaLizeth.py
@@ -0,0 +1,11 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> telefonos= {'david':"0956354824",'katty':"0954863218"}
+>>> telefonos['jorge']="0895423625"
+>>> telefonos
+{'jorge': '0895423625', 'david': '0956354824', 'katty': '0954863218'}
+>>> del telefonos['katty']
+>>> telefonos['jordan']="0789653414"
+>>> telefonos
+{'jorge': '0895423625', 'david': '0956354824', 'jordan': '0789653414'}
+>>> 
